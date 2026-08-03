f9b4b55c2eee52d995f1812a76c2b6dcd50d736a
Lan <lanlebar6@gmail.com>
2026-08-03 15:27:28 +0200

    404 page respects theme setting

--- src/views/not-found/NotFoundView.vue
+++ src/views/not-found/NotFoundView.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="flex h-full items-center justify-center overflow-auto bg-gray-50 p-4">
+  <section
+    class="flex h-full items-center justify-center overflow-auto bg-gray-50 dark:bg-gray-950 p-4"
+  >
     <div class="text-center">
-      <h1 class="text-5xl font-bold text-gray-900">404</h1>
-      <p class="mt-2 text-md text-gray-600">Page not found</p>
+      <h1 class="text-5xl font-bold text-gray-900 dark:text-gray-100">404</h1>
+      <p class="mt-2 text-md text-gray-600 dark:text-gray-400">Page not found</p>
       <div class="">
-        <RouterLink to="/" class="text-sm text-gray-500 hover:text-gray-700 transition-colors"
+        <RouterLink
+          to="/"
+          class="text-sm text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition-colors"
           >Go to vault
         </RouterLink>
       </div>
