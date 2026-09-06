50ee4b3bbe3fa70b24b8d3db40cf8452df7ce325
Lan <lanlebar6@gmail.com>
2026-09-06 22:39:18 +0200

    fix fingerprint icon

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -122,7 +122,7 @@ async function handleSubmit() {
           @click="unlock(record.username)"
         >
           <LoadingSpinner v-if="auth.isSubmitting" size="sm" />
-          <FingerprintIcon v-else class="h-[22px] w-[22px]" />
+          <FingerprintIcon v-else class="h-5 w-5 shrink-0" />
           Unlock as {{ record.username }}
         </button>
 
